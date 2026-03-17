3ab3064342feb7e5480955c1f37a84d95e018342
Jim <jimochieng64@gmail.com>
2026-03-17 13:37:22 +0300

    Made changes to pass tests

--- server/app.py
+++ server/app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to Flatiron Cars</h1>"
+    return "Welcome to Flatiron Cars"
 
 @app.route('/<model>')
 def car(model):
     if model in existing_models:
-        return f"<h2>Flatiron {model} is in our fleet!</h2>"
+        return f"Flatiron {model} is in our fleet!"
     else:
-        return f"<h2>No models called {model} exists in our catalog</h2>"
+        return f"No models called {model} exists in our catalog"
     
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
